fac2f04555718c92f574009c8fda10765044d6b8
Isac Azevedo <isacazevedoGid.uff.br>
2026-04-20 21:46:04 -0300

    Só conferindo rapidamente

--- src/app/Components/footer/real_footer.tsx
+++ src/app/Components/footer/real_footer.tsx
@@ -3,12 +3,7 @@ export default function Footer(){
         <footer className="footer">
             <div className="footer-info">
                 <p>&copy; 2026 Projeto de Comunicação Interna CS-EPS UFF. </p>
-                <p>Contato: @oisacazevedo | +55 (21) 99999-9999</p>
-            </div>
-            <div className="footer-links">
-                <a href="#">Termos de Uso</a>
-                <a href="#">Privacidade</a>
-                <a href="#">Central de Ajuda</a>
+                <p>Contato: @serioQueVcTaLendo? | +55 (21) 99999-9999</p>
             </div>
         </footer>
     )
