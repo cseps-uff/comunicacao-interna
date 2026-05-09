4beae3958a3464022a0c15700b91e56151f1dc61
Isac Azevedo Pinheiro <isacazevedo@id.uff.br>
2026-05-08 23:54:13 -0300

    N é importante

--- src/app/Components/header/falso_header.tsx
+++ src/app/Components/header/falso_header.tsx
@@ -1,9 +1,16 @@
+'use client';
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 export default function Header(){
+    const pathname = usePathname();
+    const atualpag = pathname.split('/').pop();
     return(
         <header className="topbar">
             <div className="logo">
                 <Link href="/">Comunica IEEE</Link>
+            </div>
+            <div className="topbar">
+                {atualpag}
             </div>
             <div className="user-profile">
                 <div className="avatar">I</div>
