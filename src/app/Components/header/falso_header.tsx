4a9e4dc3061237c4da3e745b17a030eecdc75b15
Isac Azevedo Pinheiro <isacazevedo@id.uff.br>
2026-05-09 01:18:12 -0300

    Implementação do modo escuro

--- src/app/Components/header/falso_header.tsx
+++ src/app/Components/header/falso_header.tsx
@@ -1,9 +1,20 @@
 'use client';
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 export default function Header(){
+
     const pathname = usePathname();
-    const atualpag = pathname.split('/').pop();
+    const atualpag = pathname.split('/').pop() || 'newslatter';
+    const { setTheme, resolvedTheme } = useTheme();
+    const [montado, setMontado] = useState(false);
+
+    useEffect(() => {
+        setMontado(true);
+    }, []);
+
+    const temaAtualEscuro = resolvedTheme === 'dark';
     return(
         <header className="topbar">
             <div className="logo">
@@ -15,6 +26,12 @@ export default function Header(){
             <div className="user-profile">
                 <div className="avatar">I</div>
             </div>
+            <button className="botao-tema"
+                onClick={() => setTheme(temaAtualEscuro ? 'light' : 'dark')}
+                title="Alternar tema"
+                >
+                {temaAtualEscuro ? '☀️ Modo Claro' : '🌙 Modo Escuro'}
+            </button>
         </header>
-    )
+    );
 }
