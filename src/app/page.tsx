cc2b25152bc2d525dbcd8d4340fe53909c2fd3e8
Isac Azevedo Pinheiro <isacazevedo@id.uff.br>
2026-06-01 19:49:22 -0300

    fazendoBomDia feito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,18 @@ import './newslatter.css';
 export default function Newsletter() {
   return (
     <div className="main#box">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia placeat eos suscipit! Incidunt deserunt ipsa reiciendis obcaecati saepe vitae commodi nobis molestiae nisi. Dolor laboriosam, facere quaerat ipsa rerum quae?</p>
+      <h1>{fazendoBomDia()}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est rerum corrupti, maiores nisi aut vitae placeat sequi sed quia voluptate deserunt earum eos eum. Necessitatibus, fugiat. Quam esse officiis iusto.</p>
     </div>
   );
 }
-
+function fazendoBomDia() {
+  const horaAtual: number = new Date().getHours();
+  if (horaAtual >= 0 && horaAtual < 12) {
+      return "Bom dia!";
+    } else if (horaAtual >= 12 && horaAtual < 18) {
+      return "Boa tarde!";
+    } else {
+      return "Boa noite!";
+    }
+}
